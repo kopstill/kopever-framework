282ac89176ed20fbc83b6e9a5cc09ccf418c010e
kopever <kop.still@gmail.com>
2020-03-12 18:18:06 +0800

    rename DemoApplication

--- kopdoctor-framework-test/src/main/java/com/kopdoctor/framework/test/DemoApplication.java
+++ kopdoctor-framework-test/src/main/java/com/kopdoctor/framework/test/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.kopdoctor")
-public class DemotApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemotApplication.class);
+        SpringApplication.run(DemoApplication.class);
     }
 
 }
