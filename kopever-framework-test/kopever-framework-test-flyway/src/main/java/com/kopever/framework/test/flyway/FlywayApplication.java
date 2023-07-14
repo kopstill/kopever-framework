2466bddfb758c8de8fe870e769892e40275f5d42
kopever <kop.still@gmail.com>
2023-07-15 00:04:40 +0400

    flyway demo

---
+++ kopever-framework-test/kopever-framework-test-flyway/src/main/java/com/kopever/framework/test/flyway/FlywayApplication.java
@@ -0,0 +1,13 @@
+package com.kopever.framework.test.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayApplication.class);
+    }
+
+}
